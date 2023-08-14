848b332f4139e51f311ab787ecfc4cf93f6b0f60
mxrgxr <mgxrcias@gmail.com>
2023-08-14 11:59:26 -0700

    render noteform and noteitems components on notes page

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,3 +1,23 @@
-export default function OrderHistoryPage() {
+import NoteForm from '../../components/NoteForm/NoteForm'
+import NoteItem from '../../components/NoteItem/NoteItem'
 
+export default function OrderHistoryPage() {
+  const [notes, setNotes] = useState([]);
+
+  //backend logic to fetch notes for user
+
+  //backend logic to save note to database
+
+  return(
+    <div>
+      <NoteForm onAddNote={handleAddNote} />
+      {notes.length === 0 ? (
+        <p>No Notes Yet</p>
+      ) : (
+        notes.map((note) => (
+          <NoteItem key={note._id} note={note} />
+        ))
+      )}
+    </div>
+  )
 }
